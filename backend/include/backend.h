d06c204ed51cfb9bf809d2b92193899cb05e58b2
MatyasZsombor <mzsombi@icloud.com>
2026-05-29 00:52:24 +0200

    chore: add linters, formatters, just command and precommit hooks

--- backend/include/backend.h
+++ backend/include/backend.h
@@ -1,19 +1,21 @@
 #pragma once
 
+// NOLINTBEGIN(modernize-use-using, cppcoreguidelines-use-enum-class, performance-enum-size)
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef enum {
-  LANG_EN = 0,
-  LANG_DE = 1,
-  LANG_FR = 3,
+    LANG_EN = 0,
+    LANG_DE = 1,
+    LANG_FR = 3,
 } Language;
 
-char *backend_greet(const char *name, Language lang);
+char* backend_greet(const char* name, Language lang);
 
-void backend_free_string(char *str);
+void backend_free_string(char* str);
 
 #ifdef __cplusplus
 }
 #endif
+// NOLINTEND(modernize-use-using, cppcoreguidelines-use-enum-class, performance-enum-size)
